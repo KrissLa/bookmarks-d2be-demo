17cb3f422b300d02c2adfad412db125d9ab3255c
KrissLa <arsavit@gmail.com>
2020-05-23 21:51:12 +0300

    User registration

--- account/views.py
+++ account/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def user_login(request):
-    """"""
+    """Вход пользователя"""
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -27,6 +27,23 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
 
+
+def register(request):
+    """Регистрация пользователя"""
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'user_form': user_form})
+
+
+
 @login_required
 def dashboard(request):
+    """Показ меню авторизованным пользователям"""
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
