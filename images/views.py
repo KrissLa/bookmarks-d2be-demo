46aedfb68283c20b9ff411072863fa49b81a6cff
KrissLa <arsavit@gmail.com>
2020-05-27 13:30:19 +0300

    ajax_required

--- images/views.py
+++ images/views.py
@@ -5,6 +5,7 @@ from .forms import ImageCreateForm
 from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
 
 # Create your views here.
 
@@ -34,6 +35,7 @@ def image_detail(request, id, slug):
                                                         'image': image})
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
