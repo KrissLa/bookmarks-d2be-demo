d1ed2ef365fc80d662fb6c3f5371c63cc440a404
KrissLa <arsavit@gmail.com>
2020-05-31 22:17:52 +0300

    total_views (redis)

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -158,3 +158,8 @@ SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
 }
+
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
